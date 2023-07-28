ba677d067f9319a8ad74fcf9f084789bcb548191
Proshanta Roy <71019355+Proshanta15@users.noreply.github.com>
2023-07-28 20:27:26 +0600

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,13 @@
+let themeToggler = document.querySelector('.theme_toggler');
+
+themeToggler.onclick = () =>{
+
+    themeToggler.classList.toggle('active');
+
+    if(themeToggler.classList.contains('active')){
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+
+}
